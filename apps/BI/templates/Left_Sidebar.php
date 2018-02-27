eacbf2592eacbc664f06afe5722e856646f64d3b
DESKTOP-KCJK1S4\mark <mandk@qq.com>
2018-02-27 08:59:52 +0800

    updata

--- apps/BI/templates/Left_Sidebar.php
+++ apps/BI/templates/Left_Sidebar.php
@@ -7,7 +7,7 @@
       <ul>
         <?php foreach($sidebar as $x=>$x_value) {?>
             <li class="has_sub">
-              <a href="<?php  echo 'http://192.168.1.33/ERPindex/apps/BI/index.php?page='.$x ?>" class="waves-effect <?php  echo $x == PAGE?'active':'' ?>"><i class="ti-home"></i> <span> <?php  echo $x_value ?> </span> </a>
+              <a href="<?php  echo BASE_PATH.'index.php?page='.$x ?>" class="waves-effect <?php  echo $x == PAGE?'active':'' ?>"><i class="ti-home"></i> <span> <?php  echo $x_value ?> </span> </a>
             </li>
         <?php } ?>
       </ul>
